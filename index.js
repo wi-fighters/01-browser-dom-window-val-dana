1071a3fb034a5d706533a3a28e4c1c9a79722f42
Fran Borg <francesca.bte@outlook.com>
2019-08-27 14:10:37 +0200

    Fix solution

--- index.js
+++ index.js
@@ -2,25 +2,22 @@ let rndNum = randomNum(0, 10);
 function randomNum(min, max) {
     result = Math.floor(Math.random() * (max - min + 1)) + min;
     return result;
-
 }
 
-console.log(randomNum);
-
 function guessingGame() {
-    let numberToGuess = randomNum;
+    let numberToGuess = rndNum;
     let count = 1;
     let guess = parseInt(prompt('Guess a number 1-10.'));
 
-    while (guess !== numberToGuess && count < 3) {
-        guess = prompt('Wrong. Try again.')
+    while ((guess !== numberToGuess) && (count < 3)) {
+        guess = parseInt(prompt('Wrong. Try again.'))
         count++;
     }
 
-    if(guess === numberToGuess && count < 3) {
-        console.log(`Success`);
+    if ((guess === numberToGuess) && (count <= 3)) {
+        console.log(`Success! Number of attempts: ${count}`);
     } else {
-        console.log(`Fail.`)
+        console.log(`Sorry, you failed to guess the number in three attempts. The number was ${numberToGuess}!`)
     }
 }
 
