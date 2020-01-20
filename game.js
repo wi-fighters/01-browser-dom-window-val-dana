a323d6eef9c99052f276df90b56c47ee3e16370f
Dana Shacham <dshacham22@gmail.com>
2020-01-20 14:31:24 +0100

    debug game

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
 const guessingGame = () => {
-    var attempts = 1;
+    let attempts = 0;
 
     while (attempts < 4) {
 
-        var start = prompt('Guess a number between 1 and 10:');
-        var randomNumber = Math.floor(Math.random() * 10) + 1;
-        var userInput = start;
+        const start = prompt('Guess a number between 1 and 10:');
+        const randomNumber = Math.floor(Math.random() * 10) + 1;
+        const inputAsNumber = parseInt(start, 10);
+        attempts += 1;
 
-        if (userInput == randomNumber) {
+        if (inputAsNumber === randomNumber) {
             alert(`Success, the number was ${randomNumber}! Attempts: ${attempts}`);
             break;
-        } else if (userInput !== randomNumber && attempts !== 3) {
-            attempts++;
+        } else if (inputAsNumber !== randomNumber && attempts !== 3) {
             alert(`Wrong!! Try again!!`);
         } else {
             alert(`Sorry, you failed to guess the number in 3 attempts. The number was ${randomNumber}.`);
